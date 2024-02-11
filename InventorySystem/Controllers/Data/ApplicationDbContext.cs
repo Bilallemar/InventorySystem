0e1bbcae21dba76add161081a134ccdf27d3c624
bilallemar <bilalarab2018@gmail.com>
2024-02-11 11:55:43 +0430

    New Changes

---
+++ InventorySystem/Controllers/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using InventorySystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventorySystem.Controllers.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options)
+           : base(options)
+        {
+
+        }
+       public DbSet<Category> Categories {  get; set; }
+    }
+}
