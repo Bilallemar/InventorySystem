8266bc550f5ecf2531f955ebe1d3e7919beace5e
bilallemar <bilalarab2018@gmail.com>
2024-02-14 12:54:15 +0430

    update

---
+++ InventorySystem/Controllers/ReportsController.cs
@@ -0,0 +1,43 @@
+﻿using InventorySystem.Controllers.Data;
+using InventorySystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace InventorySystem.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ReportsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.Reports = _db.Purchases.Include(x => x.Product).ToList();
+            ViewData["ProductId"] = new SelectList(_db.Products, "Id", "Name");
+            return View();
+        }
+
+        // POST: PurchaseReport
+        [HttpPost]
+        public async Task<IActionResult> Index([Bind("ProductId")] Purchase purchase)
+        {
+            ViewBag.Reports = _db.Purchases.Where(x => (x.ProductId == purchase.ProductId || purchase.ProductId == 0)).Include(x =>x.Product).ToList();
+
+            ViewData["ProductId"] = new SelectList(_db.Products, "Id", "Name");
+            return View();
+        }
+        // POST: PurchaseReport
+        [HttpPost]
+        public async Task<IActionResult> PrintPurchaseReport([Bind("ProductId")] Purchase purchase)
+        {
+            ViewBag.Reports = _db.Purchases.Where(x => (x.ProductId == purchase.ProductId || purchase.ProductId == 0)).Include(x => x.Product).ToList();
+
+            ViewData["ProductId"] = new SelectList(_db.Products, "Id", "Name");
+            return View();
+        }
+    }
+}
